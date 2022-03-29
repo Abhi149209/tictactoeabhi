611ee67b709016811c8d3c4afa60396ac9c252f3
Abhi149209 <sahai.abhinav99@gmail.com>
2022-03-29 19:40:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconnyComponent } from './components/iconny/iconny.component';
+import { IconComponent } from './components/icon/icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; 
+import { ToastrModule } from 'ngx-toastr'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconnyComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
